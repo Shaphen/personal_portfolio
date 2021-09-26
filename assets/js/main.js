906ebea89f53a3f1b64edf4bed76cb458566b9f9
Shaphen <swpburn@gmail.com>
2021-09-25 15:22:25 -1000

    Add modal for project displays with content and links to source code and live sites

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,71 +13,96 @@
 		$header = $('#header');
 
 	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ null,      '480px'  ]
-		});
+	breakpoints({
+		xlarge:   [ '1281px',  '1680px' ],
+		large:    [ '981px',   '1280px' ],
+		medium:   [ '737px',   '980px'  ],
+		small:    [ '481px',   '736px'  ],
+		xsmall:   [ null,      '480px'  ]
+	});
 
 	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	$window.on('load', function() {
+		window.setTimeout(function() {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
 
 	// Mobile?
-		if (browser.mobile)
+	if (browser.mobile)
+		$body.addClass('is-mobile');
+	else {
+
+		breakpoints.on('>medium', function() {
+			$body.removeClass('is-mobile');
+		});
+
+		breakpoints.on('<=medium', function() {
 			$body.addClass('is-mobile');
-		else {
+		});
 
-			breakpoints.on('>medium', function() {
-				$body.removeClass('is-mobile');
-			});
-
-			breakpoints.on('<=medium', function() {
-				$body.addClass('is-mobile');
-			});
-
-		}
+	}
 
 	// Scrolly.
-		$('.scrolly')
-			.scrolly({
-				speed: 1500,
-				offset: $header.outerHeight()
-			});
+	$('.scrolly')
+		.scrolly({
+			speed: 1500,
+			offset: $header.outerHeight()
+		});
 
 	// Menu.
-		$('#menu')
-			.append('<a href="#menu" class="close"></a>')
-			.appendTo($body)
-			.panel({
-				delay: 500,
-				hideOnClick: true,
-				hideOnSwipe: true,
-				resetScroll: true,
-				resetForms: true,
-				side: 'right',
-				target: $body,
-				visibleClass: 'is-menu-visible'
-			});
+	$('#menu')
+		.append('<a href="#menu" class="close"></a>')
+		.appendTo($body)
+		.panel({
+			delay: 500,
+			hideOnClick: true,
+			hideOnSwipe: true,
+			resetScroll: true,
+			resetForms: true,
+			side: 'right',
+			target: $body,
+			visibleClass: 'is-menu-visible'
+		});
 
 	// Header.
-		if ($banner.length > 0
-		&&	$header.hasClass('alt')) {
+	if ($banner.length > 0
+	&&	$header.hasClass('alt')) {
 
-			$window.on('resize', function() { $window.trigger('scroll'); });
+		$window.on('resize', function() { $window.trigger('scroll'); });
 
-			$banner.scrollex({
-				bottom:		$header.outerHeight() + 1,
-				terminate:	function() { $header.removeClass('alt'); },
-				enter:		function() { $header.addClass('alt'); },
-				leave:		function() { $header.removeClass('alt'); }
-			});
+		$banner.scrollex({
+			bottom:		$header.outerHeight() + 1,
+			terminate:	function() { $header.removeClass('alt'); },
+			enter:		function() { $header.addClass('alt'); },
+			leave:		function() { $header.removeClass('alt'); }
+		});
 
-		}
+	}
+
+	// modals
+	const dodgeModal = document.getElementById("dodge-modal-box")
+	const dodgeImage = document.getElementById("dodge")
+	const dodgeCloseButton = document.getElementById("dodge-modal-close")
+
+	dodgeImage.onclick = function() {
+		dodgeModal.style.display = "block"
+	}
+
+	dodgeCloseButton.onclick = function () {
+		dodgeModal.style.display = "none"
+	}
+
+	const badcordModal = document.getElementById("badcord-modal-box")
+	const badcordImage = document.getElementById("badcord")
+	const badcordCloseButton = document.getElementById("badcord-modal-close")
+
+	badcordImage.onclick = function() {
+		badcordModal.style.display = "block"
+	}
+
+	badcordCloseButton.onclick = function () {
+		badcordModal.style.display = "none"
+	}
 
 })(jQuery);
